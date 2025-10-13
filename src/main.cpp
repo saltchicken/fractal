ca40a1dd5652017ffe779c08b23a9633793735e2
saltchicken <john.eicher89@gmail.com>
2025-10-12 17:51:30 -0700

    Remove APPLE if statement

--- src/main.cpp
+++ src/main.cpp
@@ -275,9 +275,6 @@ GLFWwindow* init_window() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    #ifdef __APPLE__
-        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    #endif
 
     load_config(CONFIG_FILENAME);
 
